f8def3a8af0772b503e397985d5f7ec019b8c8f6
linhhust <tuanlinh10c@gmail.com>
2021-05-21 22:41:06 +0700

    add

---
+++ app/Events/UpdateOption.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Log;
+class UpdateOption implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+    public $option, $match;
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($option, $match)
+    {
+        $this->option = $option;
+        $this->match = $match;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return ['update-option'];
+    }
+}
---
+++ public/js/home.js
@@ -0,0 +1,28 @@
+var channel_update_option = pusher.subscribe('update-option');
+var channel_user_prediction = pusher.subscribe('user-prediction');
+
+channel_update_option.bind('App\\Events\\UpdateOption', function(data) {
+    var option = $(`.bet_option_${data.option.id}`)
+    $(option).find('a').data('team-name', data.option.option_name);
+    $(option).find('a').data('confrontation', data.match.name);
+    $(option).find('a').data('id', data.option.id);
+    $(option).find('a').data('minamo', data.option.min_amo);
+    $(option).find('a').data('macthid', data.match.id);
+    $(option).find('a').data('ratioone', data.option.ratio1);
+    $(option).find('a').data('ratiotwo', data.option.ratio2);
+    $(option).find('a').data('betlimit', data.option.bet_limit);
+    $(option).find('a').data('questionid', data.option.ratio1 + " : " + data.option.ratio2);
+    $(option).find('a').data('wager-count', data.option.option_name);
+    $(option).find('a').data('option_name', data.option.option_name);
+    $(option).find('.option-name').text(data.option.option_name);
+    $(option).find('.sport-table-wager-button-count').text(data.option.ratio1 + " : " + data.option.ratio2);
+    // $(option).find('.progress-bar').text(percent(data.totalInvestByOption, data.totalInvest) + "%");
+});
+
+function percent(num_amount, num_total) {
+    if (num_total > 0) {
+        return Math.round(num_amount * 100 / num_total);
+    } else {
+        return 0; // or whatever you want
+    }
+}
